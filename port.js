8533916510067f8a3765ac506027c051af969004
thesykap <theresekapinga047@gmail.com>
2026-01-27 10:29:46 -0500

    Clean code and fix responsive layout issues

--- port.js
+++ port.js
@@ -1,3 +1,11 @@
 const image = document.querySelector("#menu");
 const affichs = document.querySelector("#menu-btn");
-affichs.addEventListener("click",()=>image.classList.toggle("active"))
+affichs.addEventListener("click",()=>{
+    image.classList.toggle("active");
+    document.body.classList.toggle("navbar-hidden"); /* pour que la section prennent 100% de la largeur*/
+    if(image.classList.contains("active")){
+        affichs.textContent = "✖";
+    }else{
+        affichs.textContent = "☰";
+    }
+})
